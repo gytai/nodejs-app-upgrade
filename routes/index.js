1eb276362a478773ab338267dd142ab0dc5a82d0
邰广银 <273650669@qq.com>
2017-09-14 20:35:44 +0800

    base code

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.session.user){
-      res.render('index', { title: 'Express' });
+      res.render('index');
   }else{
       res.render('login');
   }
 });
 
 
-router.get('/index', function(req, res, next) {
-    res.sendfile("./template/index.html");
+router.get('/project', function(req, res, next) {
+    res.render('project');
 });
 
 module.exports = router;
